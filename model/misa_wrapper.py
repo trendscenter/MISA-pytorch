281b3c22d6606e9c9f58bdcb2b23bfb1c4480364
DKhosravinezhad <dkhosravinezhad@gmail.com>
2022-07-13 12:53:44 -0400

    update MISA wrapper

--- model/misa_wrapper.py
+++ model/misa_wrapper.py
@@ -5,8 +5,8 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-# import .model.MISAK MISA
+import .model.MISAK MISA
 
 def MISA_wrapper(latent_dim, n_layers, lr, seed,
                  ckpt_file='misa.pt', test=False):
-    pass
+    model = MISA(weights = list(), index = index, subspace = subspace, beta = beta, eta = eta, lam = lam, input_dim = input_dim, output_dim = output_dim)
