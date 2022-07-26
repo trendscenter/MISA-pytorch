26c96be4295115f148b993aa69872f01894d1dd8
DKhosravinezhad <dkhosravinezhad@gmail.com>
2022-07-26 17:14:08 -0400

    wrapper update

--- model/misa_wrapper.py
+++ model/misa_wrapper.py
@@ -26,13 +26,13 @@ def MISA_wrapper(data_loader, index, subspace, eta, beta, lam, input_dim, output
         model.predict(data_loader)
     
     torch.save({'model': model.state_dict(),
-                'seed': model.seed.detach().cpu(),
-                'index': model.index.detach().cpu(),
-                'subspace': model.subspace.detach().cpu(),
-                'eta': model.eta.detach().cpu(),
-                'beta': model.beta.detach().cpu(),
-                'lam': model.lam.detach().cpu()},
+                'seed': model.seed,
+                'index': model.index,
+                'subspace': model.subspace,
+                'eta': model.eta,
+                'beta': model.beta,
+                'lam': model.lam},
                ckpt_file)
     print("Saved to: " + ckpt_file)
     
-    return model.output.detach().cpu()
+    return model.output
